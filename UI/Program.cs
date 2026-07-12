cba919f88760ed2a20972f7c231cfe68cbcf8783
Nia Tsalkalamanidze <tsalkalamanidzenia93@gmail.com>
2026-07-12 13:13:15 +0400

    main

--- UI/Program.cs
+++ UI/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("ATM System started!");
+            Console.WriteLine("ATM System starteed!");
         }
     }
 }
